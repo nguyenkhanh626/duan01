c17b79edf27530b0106afd7a12ca1348934bf433
nguyenkhanh626 <7799khanh@gmail.com>
2025-11-03 00:45:20 +0700

    123

--- qly/src/NhanVien.java
+++ qly/src/NhanVien.java
@@ -1,10 +1,5 @@
-/**
- * Lớp (model) đại diện cho đối tượng Nhân viên.
- * PHIÊN BẢN CẬP NHẬT: Thêm 'diemThuongDuAn' (điểm thưởng dự án).
- */
 public class NhanVien {
 
-    // Thuộc tính cũ
     private String maNhanVien;
     private String hoTen;
     private String phongBan;
@@ -14,11 +9,8 @@ public class NhanVien {
     private String cccd;
     private int thamNien;
     private int diemViPham;
-
-    // Thuộc tính mới cho tab Dự án
     private int diemThuongDuAn;
 
-    // Constructor (phương thức khởi tạo)
     public NhanVien(String maNhanVien, String hoTen, String phongBan, String sdt, String email, String ngaySinh, String cccd, int thamNien) {
         this.maNhanVien = maNhanVien;
         this.hoTen = hoTen;
@@ -32,7 +24,6 @@ public class NhanVien {
         this.diemThuongDuAn = 0; // TỰ ĐỘNG KHỞI TẠO LÀ 0
     }
 
-    // === Getters (Cũ) ===
     public String getMaNhanVien() { return maNhanVien; }
     public String getHoTen() { return hoTen; }
     public String getPhongBan() { return phongBan; }
@@ -42,8 +33,8 @@ public class NhanVien {
     public String getCccd() { return cccd; }
     public int getThamNien() { return thamNien; }
     public int getDiemViPham() { return diemViPham; }
+    public int getDiemThuongDuAn() { return diemThuongDuAn; }
     
-    // === Setters (Cũ) ===
     public void setMaNhanVien(String maNhanVien) { this.maNhanVien = maNhanVien; }
     public void setHoTen(String hoTen) { this.hoTen = hoTen; }
     public void setPhongBan(String phongBan) { this.phongBan = phongBan; }
@@ -55,29 +46,14 @@ public class NhanVien {
     public void addDiemViPham(int points) { this.diemViPham += points; }
     public void setDiemViPham(int diemViPham) { this.diemViPham = diemViPham; }
 
-    // === PHƯƠNG THỨC MỚI CHO ĐIỂM THƯỞNG DỰ ÁN ===
-
-    /**
-     * Lấy điểm thưởng dự án hiện tại
-     */
-    public int getDiemThuongDuAn() {
-        return diemThuongDuAn;
-    }
-
-    /**
-     * Cộng điểm thưởng khi được thêm vào dự án
-     */
+    
     public void addDiemThuongDuAn(int diem) {
         this.diemThuongDuAn += diem;
     }
 
-    /**
-     * Đặt lại điểm thưởng (ví dụ: cuối năm)
-     */
     public void setDiemThuongDuAn(int diem) {
         this.diemThuongDuAn = diem;
     }
-
 
     @Override
     public String toString() {
