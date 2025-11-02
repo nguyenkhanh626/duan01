fa43ca2f42974e031feb8a6245fea8567e1ee005
nguyenkhanh626 <7799khanh@gmail.com>
2025-11-03 00:43:59 +0700

    123

--- qly/src/DuAn.java
+++ qly/src/DuAn.java
@@ -1,19 +1,15 @@
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Lớp (model) mới đại diện cho đối tượng Dự án.
- */
+
 public class DuAn {
 
     private String maDuAn;
     private String tenDuAn;
     private int doPhucTap; // 1, 2, hoặc 3
 
-    // Danh sách nhân viên tham gia dự án này (quan hệ Many-to-Many)
     private List<NhanVien> danhSachThanhVien;
 
-    // Constructor
     public DuAn(String maDuAn, String tenDuAn, int doPhucTap) {
         this.maDuAn = maDuAn;
         this.tenDuAn = tenDuAn;
@@ -21,7 +17,7 @@ public class DuAn {
         this.danhSachThanhVien = new ArrayList<>(); // Khởi tạo list
     }
 
-    // Getters
+    
     public String getMaDuAn() {
         return maDuAn;
     }
@@ -38,7 +34,6 @@ public class DuAn {
         return danhSachThanhVien;
     }
 
-    // Setters
     public void setTenDuAn(String tenDuAn) {
         this.tenDuAn = tenDuAn;
     }
@@ -47,17 +42,14 @@ public class DuAn {
         this.doPhucTap = doPhucTap;
     }
 
-    // Phương thức logic để thêm thành viên
     public void addThanhVien(NhanVien nv) {
         this.danhSachThanhVien.add(nv);
     }
     
-    // Kiểm tra xem nhân viên đã có trong dự án chưa
     public boolean hasThanhVien(NhanVien nv) {
         return this.danhSachThanhVien.contains(nv);
     }
 
-    // Dùng để hiển thị tên trong JComboBox
     @Override
     public String toString() {
         return this.tenDuAn;
